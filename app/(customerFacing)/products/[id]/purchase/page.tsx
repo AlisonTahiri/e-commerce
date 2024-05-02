8bcd13c9c7bbea6e2cc6b9fef9ed295262c9c82e
Alison Tahiri <alison.tahiri@gmail.com>
2024-05-02 12:06:08 +0200

    Add users product purchase page

--- app/(customerFacing)/products/[id]/purchase/page.tsx
+++ app/(customerFacing)/products/[id]/purchase/page.tsx
@@ -1,6 +1,10 @@
 import db from "@/app/db/db";
 import { notFound } from "next/navigation";
 import React from "react";
+import Stripe from "stripe";
+import CheckoutForm from "./_components/CheckoutForm";
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY as string);
 
 export default async function PurchasePage({
   params: { id },
@@ -13,5 +17,20 @@ export default async function PurchasePage({
 
   if (product == null) return notFound();
 
-  return <div>Purchase</div>;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount: product.priceInCents,
+    currency: "USD",
+    metadata: { productId: product.id },
+  });
+
+  if (paymentIntent.client_secret == null) {
+    throw new Error("Stripe failed to create payment intent.");
+  }
+
+  return (
+    <CheckoutForm
+      product={product}
+      clientSecret={paymentIntent.client_secret}
+    />
+  );
 }
